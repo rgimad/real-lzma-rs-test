eb7f42853be2f87f91f3bde606fbeab38ef598e7
rgimad <33692565+rgimad@users.noreply.github.com>
2025-05-19 00:36:13 +0300

    unpacking works tanks Max BUT why we need extra 0-byte in header and why swap the dword in header?????

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::path::Path;
-use std::io::{BufReader, Cursor};
+use std::io::{BufReader, Cursor, Write};
 use std::time::Duration;
 use byteorder::{LittleEndian, ReadBytesExt};
 use std::fs::File;
@@ -12,6 +12,17 @@ fn push_u32_le(vec: &mut Vec<u8>, value: u32) {
     vec.push(((value >> 8) & 0xFF) as u8);
     vec.push(((value >> 16) & 0xFF) as u8);
     vec.push(((value >> 24) & 0xFF) as u8); // Most significant byte
+}
+
+fn push_u64_le(vec: &mut Vec<u8>, value: u64) {
+    vec.push((value & 0xFF) as u8);         // Least significant byte
+    vec.push(((value >> 8) & 0xFF) as u8);
+    vec.push(((value >> 16) & 0xFF) as u8);
+    vec.push(((value >> 24) & 0xFF) as u8);
+    vec.push(((value >> 32) & 0xFF) as u8);
+    vec.push(((value >> 40) & 0xFF) as u8);
+    vec.push(((value >> 48) & 0xFF) as u8);
+    vec.push(((value >> 56) & 0xFF) as u8);
 }
 
 fn push_u32_be(vec: &mut Vec<u8>, value: u32) {
@@ -26,14 +37,6 @@ fn push_u32_le_alt(vec: &mut Vec<u8>, value: u32) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-
-
-    let mut comp: Vec<u8> = Vec::new();
-    lzma_rs::lzma_compress(&mut std::io::BufReader::new("textazazzazaa".as_bytes()), &mut comp).unwrap();
-    println!("comp = {:X?} ", &comp);
-
-
-
 
     let file_path = "nix.skn";
 
@@ -55,7 +58,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let lzma_props: u8 = (2*5+0)*9+3;
     let mut lzma_dict_size = 0x10000u32;
-    let lzma_unpack_size = unpack_size;
+    let lzma_unpack_size = unpack_size as u64;
 
     while lzma_dict_size < unpack_size {
         lzma_dict_size *= 2;
@@ -68,18 +71,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     // push_u32_le(&mut lzma_buf, 0x800000);
     push_u32_le(&mut lzma_buf, lzma_dict_size);
 
-    push_u32_le(&mut lzma_buf, 0xFFFFFFFF);
-    push_u32_le(&mut lzma_buf, 0xFFFFFFFF);
-
-    // push_u32_le(&mut lzma_buf, lzma_unpack_size);
-    // push_u32_le(&mut lzma_buf, 0);
-
-    // lzma_buf.push(0);
-
-    // let tmp = cursor1.read_u32::<LittleEndian>()?;
-    // push_u32_be(&mut lzma_buf, tmp);
+    push_u64_le(&mut lzma_buf, lzma_unpack_size);
+    lzma_buf.push(0 as u8);  // TODO: WHY?
 
     lzma_buf.extend_from_slice(&file_data[4*3..]);
+
+    let data_offset = 14;
+    // TODO: WHY? ?
+    lzma_buf.swap(data_offset+0, data_offset+3);
+    lzma_buf.swap(data_offset+1, data_offset+2);
 
     println!("lzma_buf = {:X?} ", &lzma_buf);
 
@@ -89,14 +89,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     //let mut lzma_buf_reader = BufReader::new(lzma_buf.as_slice());
     //lzma_rs::lzma_decompress(&mut lzma_buf_reader, &mut unpacked_data).unwrap();
     lzma_rs::lzma_decompress(&mut lzma_buf.as_slice(), &mut unpacked_data).unwrap();
-        
-
-    // let mut f = std::io::BufReader::new(std::fs::File::open(filename).unwrap());
-
-    // let mut decomp: Vec<u8> = Vec::new(); // can be anything that implements "std::io::Write"
-    // lzma_rs::lzma_decompress(&mut f, &mut decomp).unwrap();
-
-    // Decompressed content is now in "decomp"
+    
+    let mut file = File::create("nix_unpacked.skn")?; // Create the file (or overwrite if it exists)
+    file.write_all(&unpacked_data)?;
 
     Ok(())
 }
